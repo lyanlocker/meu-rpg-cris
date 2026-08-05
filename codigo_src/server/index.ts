02d2362c24f7557a186329e779bdc35140417cbb
lyanlocker <lyanlocker@gmail.com>
2026-08-04 21:53:49 -0300

    Permite uploads compactados de retratos

--- codigo_src/server/index.ts
+++ codigo_src/server/index.ts
@@ -14,13 +14,14 @@ declare module "http" {
 
 app.use(
   express.json({
+    limit: "3mb",
     verify: (req, _res, buf) => {
       req.rawBody = buf;
     },
   }),
 );
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false, limit: "3mb" }));
 
 export function log(message: string, source = "express") {
   const formattedTime = new Date().toLocaleTimeString("en-US", {
@@ -88,7 +89,6 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on the port specified in the environment variable PORT
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
-  // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
   httpServer.listen(
     {
